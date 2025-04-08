e115d6c0f177fa2f8871729297bc24e27455bca5
Hor Kimseur <horkimseur21@kit.edu.kh>
2025-04-08 19:24:14 +0700

    update

--- components/TopicList.jsx
+++ components/TopicList.jsx
@@ -4,50 +4,45 @@ import Link from 'next/link'
 import { HiPencilAlt } from 'react-icons/hi'
 
 
-const getTopics = async () => {
+const getTopics = async()=>{
   try {
     const res = await fetch('http://localhost:3000/api/topics', {
-      next: { revalidate: 10 }, // Revalidate every 10 seconds
-    });
+      cache: 'no-store',
+    })
 
-    if (!res.ok) {
-      throw new Error('Failed to fetch data');
+    if(!res.ok){
+      throw new Error('Failed to fetch data')
     }
     return res.json();
+
   } catch (error) {
-    console.log(error);
-    return { topics: [] }; // Return an empty array as a fallback
+    console.log(error)
   }
-};
+}
 
 export default async function TopicList() {
-  const data = await getTopics();
-  const topics = data?.topics || []; // Fallback to an empty array if topics is undefined
+  const { topics } = await getTopics()
 
   return (
     <>
-      {topics.length > 0 ? (
-        topics.map((t) => (
-          <div
-            key={t._id}
-            className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
-          >
-            <div>
-              <h2 className="font-bold text-2xl">{t.title}</h2>
-              <div>{t.description}</div>
-            </div>
-
-            <div className="flex gap-2">
-              <RemoveBtn id={t._id} />
-              <Link href={`/editTopic/${t._id}`}>
-                <HiPencilAlt size={24} />
-              </Link>
-            </div>
+      {topics.map((t) => (
+        <div
+          key={t._id} // Moved the key prop here
+          className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
+        >
+          <div>
+            <h2 className="font-bold text-2xl">{t.title}</h2>
+            <div>{t.description}</div>
           </div>
-        ))
-      ) : (
-        <div>No topics available</div> // Fallback UI
-      )}
+
+          <div className="flex gap-2">
+            <RemoveBtn id={t._id} />
+            <Link href={`/editTopic/${t._id}`}>
+              <HiPencilAlt size={24} />
+            </Link>
+          </div>
+        </div>
+      ))}
     </>
-  );
+  )
 }
